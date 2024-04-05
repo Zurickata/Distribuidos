b2ca6b3ca6a1a5b03c94be82a209360d21d7d73d
Zurickata <80793095+Zurickata@users.noreply.github.com>
2024-04-04 21:26:01 -0300

    Entrega Final

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -1,56 +1,81 @@
 package main
 
-import (    
+import (
 	"fmt"
+	"math/rand"
 	"net"
-    "strings"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Planet struct {
-	Name    string 
-	Booty   int    
-	Captain string 
+	Name    string
+	Booty   int
+	Captain string
 }
 
 func main() {
-    planet := "Pluton"
-    booty := 10
-    captain := "C1"
+	// Definir el número máximo de iteraciones y el intervalo de tiempo entre solicitudes
+	maxIterations := 2
 
-    message := fmt.Sprintf("%s:%d:%s", planet, booty, captain)
-    sendRequest(message)
+	// Definir el rango específico para los datos aleatorios
+	minBooty := 1
+	maxBooty := 20
+
+	// Establecer una semilla aleatoria
+	rand.Seed(time.Now().UnixNano())
+
+	// Bucle para enviar solicitudes repetidamente
+	for i := 0; i < maxIterations; i++ {
+
+		planet := string(rune('A' + rand.Intn(6)))
+		booty := rand.Intn(maxBooty-minBooty+1) + minBooty
+		captain := "C" + strconv.Itoa(rand.Intn(3)+1)
+
+		message := fmt.Sprintf("%s:%d:%s", planet, booty, captain)
+		sendRequest(message)
+
+		// Generar un intervalo aleatorio entre 2 y 7 segundos
+		interval := time.Duration(rand.Intn(6)+2) * time.Second
+		
+		// Esperar el intervalo de tiempo antes de enviar la próxima solicitud
+		time.Sleep(interval)
+	}
+
+	// En teoría deberían salir la solicitudes de manera aleatoria, no de forma secuencial cada "interval" de tiempo.
 }
 
 func sendRequest(message string) {
-    conn, err := net.Dial("tcp", "localhost:8080")
-    if err != nil {
-        fmt.Println("Error al conectar con el servidor:", err)
-        return
-    }
-    defer conn.Close()
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error al conectar con el servidor:", err)
+		return
+	}
+	defer conn.Close()
 
-    // Enviar mensaje al servidor
-    _, err = conn.Write([]byte(message))
+	// Enviar mensaje al servidor
+	_, err = conn.Write([]byte(message))
 
 	parts := strings.Split(message, ":")
 	captain := parts[2]
 	planetName := parts[0]
 	fmt.Printf("Capitán %s encontró botín en Planeta %s, enviando solicitud de asignación...\n", captain, planetName)
 
-    if err != nil {
-        fmt.Println("Error al enviar el mensaje:", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("Error al enviar el mensaje:", err)
+		return
+	}
 
-    // Leer respuesta del servidor
-    buffer := make([]byte, 1024)
-    n, err := conn.Read(buffer)
-    if err != nil {
-        fmt.Println("Error al leer la respuesta del servidor:", err)
-        return
-    }
-    response := string(buffer[:n])
+	// Leer respuesta del servidor
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error al leer la respuesta del servidor:", err)
+		return
+	}
+	response := string(buffer[:n])
 
-    // Imprimir respuesta del servidor
-    fmt.Println("Respuesta del servidor:", response)
+	// Imprimir respuesta del servidor
+	fmt.Println("Respuesta del servidor:", response)
 }
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "strconv"
-    "strings"
-    "sync"
+	"fmt"
+	"math/rand"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Planet struct {
-	Name    string 
-	Booty   int    
-	Captain string 
+	Name    string
+	Booty   int
+	Captain string
 }
 
 var planets = make(map[string]int)
@@ -38,64 +40,80 @@ func main() {
 
 func initializePlanets() {
 	// Inicializar planetas con botines aleatorios
+
+	rand.Seed(time.Now().UnixNano())
+
 	for i := 0; i < 6; i++ {
-		planetName := string('A' + i)
-		planets[planetName] = 0 // Inicialmente, ningún botín asignado
+		planetName := string(rune('A' + i))
+		planets[planetName] = rand.Intn(11)
 	}
+
+	printPlanetStatus()
 }
 
 func handleConnection(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 
-    // Leer mensaje del cliente
-    buffer := make([]byte, 1024)
-    n, err := conn.Read(buffer)
-    if err != nil {
-        fmt.Println("Error al leer el mensaje del cliente:", err)
-        return
-    }
-    message := string(buffer[:n])
+	// Leer mensaje del cliente
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error al leer el mensaje del cliente:", err)
+		return
+	}
+	message := string(buffer[:n])
 
+	// Descompone el mensaje del cliente en una Lista
+	parts := strings.Split(message, ":")
 
-    // Descompone el mensaje del cliente en una Lista
-    parts := strings.Split(message, ":")
-
-    if len(parts) != 3 {
-        fmt.Println("Mensaje del cliente con formato incorrecto:", message)
-        return
-    }
-    planetName := parts[0]
-    booty, err := strconv.Atoi(parts[1])
-    if err != nil {
-        fmt.Println("Error al convertir botín a entero:", err)
-        return
-    }
-    captain := parts[2]
+	if len(parts) != 3 {
+		fmt.Println("Mensaje del cliente con formato incorrecto:", message)
+		return
+	}
+	planetName := parts[0]
+	booty, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println("Error al convertir botín a entero:", err)
+		return
+	}
+	captain := parts[2]
 
 	fmt.Printf("Recepción de solicitud desde el Planeta %s, del capitan %s\n", planetName, captain)
 
-    // Asignar botín al planeta adecuado
-    assignBooty(planetName, booty, captain)
+	// Asignar botín al planeta adecuado
+	assignBooty(planetName, booty, captain)
 
-    // Responder al cliente con el planeta asignado
-    response := fmt.Sprintf("Botín asignado a planeta %s para capitán %s\n", planetName, captain)
-    conn.Write([]byte(response))
+	// Mostrar estado actual de las asignaciones de los planetas
+	printPlanetStatus()
+
+	// Responder al cliente con el planeta asignado
+	response := fmt.Sprintf("Botín asignado a planeta %s para capitán %s\n", planetName, captain)
+	conn.Write([]byte(response))
 }
 
 func assignBooty(planetName string, booty int, captain string) {
-    // Lógica para asignar botín al planeta adecuado
-    // (en este caso, simplemente se asigna al planeta con menos botines)
-    minBooty := 9999
-    var targetPlanet string
-    for name, booty := range planets {
-        if booty < minBooty {
-            minBooty = booty
-            targetPlanet = name
+	// Lógica para asignar botín al planeta adecuado
+	// (en este caso, simplemente se asigna al planeta con menos botines)
+	minBooty := 9999
+	var targetPlanet string
+	for name, booty := range planets {
+		if booty < minBooty {
+			minBooty = booty
+			targetPlanet = name
 
 			fmt.Printf("Botín asignado al Planeta %s, cantidad actual: %d\n", targetPlanet, minBooty)
-        }
-    }
-    mutex.Lock()
-    defer mutex.Unlock()
-    planets[targetPlanet] += booty
+		}
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	planets[targetPlanet] += booty
+}
+
+func printPlanetStatus() {
+	// Imprimir estado actual de las asignaciones de los planetas
+	status := "Estado actual de las asignaciones de los planetas: "
+	for name, booty := range planets {
+		status += fmt.Sprintf("P%s: %d, ", name, booty)
+	}
+	fmt.Println(status[:len(status)-2]) // Eliminar la última coma y espacio
 }
